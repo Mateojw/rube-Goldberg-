9db5dbce899b5cc053cdc22b4439e8f2331a8059
DESKTOP-2GCSJNI\Mateo <mjw557@nyu.edu>
2016-11-14 23:14:40 -0500

    Physics Rube Goldberg : lost of colors lots of fun ! ( had some trouble with rigidbody.useGravity = true not working but other than that everything runs smooth.

---
+++ New Unity Project/Assets/scripts/CameraControllerClass.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControllerClass : MonoBehaviour {
+
+	//public GameObject Camera1;
+	//public GameObject Camera2;
+
+	public GameObject[] Cameras;
+	private int currentCam;
+
+
+	// Use this for initialization
+	void Start () {
+		// make camera 1 active by default;
+		//Camera1.SetActive(true);
+		//Camera2.SetActive(false);
+
+		DeactivateCameras();
+		Cameras[0].SetActive(true);
+		currentCam = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+
+	}
+
+	public void activateCamera(int _camNumber){
+	
+		Cameras[_camNumber - 1].SetActive(true);
+		currentCam = _camNumber - 1;
+	}
+
+	public void DeactivateCameras()
+	{
+		for (int i = 0; i < Cameras.Length; i++)
+		{
+			Cameras[i].SetActive(false);
+		}
+
+	}
+}
---
+++ New Unity Project/Assets/scripts/gravitychange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravitychange : MonoBehaviour {
+
+	
+
+	
+    void onMouseDown()
+    { 
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          
+
+        }
+
+    }
+}
